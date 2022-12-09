e469c23018b1168f58926d5b8c463e02cede065b
Moley93 <joshmole@me.com>
2022-12-09 04:59:26 -0800

    Initial commit

---
+++ [cfx-default]/[gameplay]/chat/__resource.lua
@@ -0,0 +1,36 @@
+<<<<<<< HEAD:[cfx-default]/[gameplay]/chat/__resource.lua
+description 'chat management stuff'
+
+ui_page 'html/index.html'
+=======
+-- This resource is part of the default Cfx.re asset pack (cfx-server-data)
+-- Altering or recreating for local use only is strongly discouraged.
+
+version '1.0.0'
+author 'Cfx.re <root@cfx.re>'
+description 'Provides baseline chat functionality using a NUI-based interface.'
+repository 'https://github.com/citizenfx/cfx-server-data'
+
+ui_page 'dist/ui.html'
+>>>>>>> parent of 056af5fc (chat):[cfx-default]/[gameplay]/chat/fxmanifest.lua
+
+client_script 'cl_chat.lua'
+server_script 'sv_chat.lua'
+
+files {
+  'dist/ui.html',
+  'dist/index.css',
+  'html/vendor/*.css',
+  'html/vendor/fonts/*.woff2',
+}
+
+fx_version 'adamant'
+games { 'rdr3', 'gta5' }
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+dependencies {
+  'yarn',
+  'webpack'
+}
+
+webpack_config 'webpack.config.js'
---
+++ [standalone]/loadingscreen/README.md
@@ -0,0 +1,5 @@
+Discord : https://discord.gg/C3aN2bBVbB
+
+<img src="https://i.imgur.com/wznR5im.png">
+
+Vidéo : https://youtu.be/jwwKqRf3Xio
