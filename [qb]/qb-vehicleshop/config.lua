4ac043cf213321e8663e5d61987ea20dadc505ed
Moley93 <joshmole@me.com>
2022-12-09 10:08:25 -0800

    pdm car configs

--- [qb]/qb-vehicleshop/config.lua
+++ [qb]/qb-vehicleshop/config.lua
@@ -38,27 +38,27 @@ Config.Shops = {
         ['TestDriveSpawn'] = vector4(-56.79, -1109.85, 26.43, 71.5), -- Spawn location for test drive
         ['ShowroomVehicles'] = {
             [1] = {
-                coords = vector4(-49.73, -1083.82, 27.3, 125.86), -- where the vehicle will spawn on display
+                coords = vector4(-49.73, -1083.82, 25.95, 125.86), -- where the vehicle will spawn on display
                 defaultVehicle = 'adder', -- Default display vehicle
                 chosenVehicle = 'adder', -- Same as default but is dynamically changed when swapping vehicles
             },
             [2] = {
-                coords = vector4(-54.47, -1096.82, 26.44, 104.44),
+                coords = vector4(-54.47, -1096.82, 25.95, 104.44),
                 defaultVehicle = 'schafter2',
                 chosenVehicle = 'schafter2'
             },
             [3] = {
-                coords = vector4(-47.42, -1091.99, 26.25, 71.16),
+                coords = vector4(-47.42, -1091.99, 25.95, 71.16),
                 defaultVehicle = 'comet2',
                 chosenVehicle = 'comet2'
             },
             [4] = {
-                coords = vector4(-36.9, -1093.12, 26.75, 110.44),
+                coords = vector4(-36.9, -1093.12, 25.95, 110.44),
                 defaultVehicle = 'vigero',
                 chosenVehicle = 'vigero'
             },
             [5] = {
-                coords = vector4(-42.13, -1101.33, 25.85, 100.76),
+                coords = vector4(-42.13, -1101.33, 25.95, 100.76),
                 defaultVehicle = 't20',
                 chosenVehicle = 't20'
             },
