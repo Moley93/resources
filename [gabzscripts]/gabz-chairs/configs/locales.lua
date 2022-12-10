419f196198e9d166341a0c3ae33b9d3432c68e00
Moley93 <joshmole@me.com>
2022-12-10 04:57:55 -0800

    gabz-doorlock changes

--- [gabzscripts]/gabz-chairs/configs/locales.lua
+++
@@ -1,8 +0,0 @@
-Locales = { 
-    ['EN'] = { --english
-        ['command_control'] = 'Sit/lie down and to stand up',
-        ['command_control_right'] = 'Change animation while you are sat/laying',
-        ['command_control_left'] = 'Change animation while you are sat/laying',
-        ['command_cancel'] = 'Cancel emotes',
-    },
-}
