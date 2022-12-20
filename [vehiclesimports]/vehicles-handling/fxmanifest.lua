7b6e95af058987b389c21dab6f7a37a70f4b378d
Moley93 <joshmole@me.com>
2022-12-20 12:24:30 +0000

    GTA Vehicle Handling Files - Unique Handling

---
+++ [vehiclesimports]/vehicles-handling/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+game 'gta5'
+
+files {
+	'data/*.meta',
+	'data/**/*.meta',
+}
+
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+
+data_file 'VEHICLE_METADATA_FILE' 'data/*_vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/*_carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/*_carvariations.meta'
+
+data_file 'HANDLING_FILE' 'data/**/*_handling.meta'
+
+client_scripts {
+	'vehicle-names.lua',
+}
