7b78a98da540de989abad5f62aff10b3dc96381a
Moley93 <joshmole@me.com>
2022-12-27 18:25:38 +0000

    Added PD Apply and Complaint Forms to Front Desk

--- [standalone]/ApplyPDForm/config.lua
+++ [standalone]/ApplyPDForm/config.lua
@@ -1,10 +1,10 @@
 Config = {}
 Config.UseEButton = true
-Config.Webhook = "https://discord.com/api/webhooks/1057319651715055707/cCpaSeS4GtU3pJfEI1Kjz7uBy7N4NLR3uIcdAf2pVqdhCbN4Q7mSxEv3cXKXkP_bfCWm"
+Config.Webhook = "https://discord.com/api/webhooks/1057362552662790235/ixOZe48Ba7_JoumvrODueCbcVQhNxDQrNnoXUu4Do83APdzdHBZuF31p8g1KGLaVnm73"
 Config.WebhookName = "Police Forms"
 Config.HookBottomText = "Police"
 Config.FormLocation = {
-    vector3(441.91, -979.55, 30.69)
+    vector3(441.78, -984.29, 30.69)
 }
 
 -- open form event if using Target 
