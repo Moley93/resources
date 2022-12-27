b0af4da1617ed4d9f8035f084888d50aa33ff3af
Moley93 <joshmole@me.com>
2022-12-27 15:32:34 +0000

    Police Complaint Box

---
+++ [standalone]/ComplaintForm/config.lua
@@ -0,0 +1,14 @@
+Config = {}
+Config.UseEButton = true
+Config.Webhook = "https://discord.com/api/webhooks/1057319651715055707/cCpaSeS4GtU3pJfEI1Kjz7uBy7N4NLR3uIcdAf2pVqdhCbN4Q7mSxEv3cXKXkP_bfCWm"
+Config.WebhookName = "Police Forms"
+Config.HookBottomText = "Police"
+Config.FormLocation = {
+    vector3(1098.37, -2013.18, 30.89)
+}
+
+-- open form event if using Target 
+
+-- TriggerEvent('police:form:openForm')
+
+-- This event i sclient sided
