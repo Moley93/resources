16fb9b39e53e87741169ba8cfd66b3b684e80739
Moley93 <joshmole@me.com>
2023-01-03 19:52:33 +0000

    Services

---
+++ [standalone]/npwd_services/config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.Jobs = {
+    "mechanic",
+    "cardealer",
+    "realestate",
+    "taxi",
+    "bus",
+    "lawyer",
+    "tow",
+}
+
+Config.ShowOffDuty = false --show players who aren't currently on duty (QBcore Only)
