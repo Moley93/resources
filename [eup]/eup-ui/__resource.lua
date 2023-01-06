fa9a95ee1a7a51fff0a03550a5436ef8046d92e6
Moley93 <joshmole@me.com>
2023-01-06 23:50:52 +0000

    Police EUP Update

--- [eup]/eup-ui/__resource.lua
+++
@@ -1,6 +0,0 @@
-server_script "EV.lua"
-client_script "EV.lua"
-resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
-
-client_script "@NativeUI/NativeUI.lua"
-client_script "eup_ui.lua"
