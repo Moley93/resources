28511fc3e0781307c22a97c4f197cd11c818316c
Moley93 <joshmole@me.com>
2023-01-13 08:41:04 +0000

    Rich Presence Changes

--- [qb]/qb-smallresources/client/discord.lua
+++ [qb]/qb-smallresources/client/discord.lua
@@ -8,21 +8,21 @@ CreateThread(function()
         SetDiscordAppId()
 
         -- Here you will have to put the image name for the "large" icon.
-        SetDiscordRichPresenceAsset('logo_name')
+        SetDiscordRichPresenceAsset('nrlargeogotrans')
 
         -- (11-11-2018) New Natives:
 
         -- Here you can add hover text for the "large" icon.
-        SetDiscordRichPresenceAssetText('This is a lage icon with text')
+        SetDiscordRichPresenceAssetText('Join Near Reality with the below buttons!')
 
         -- Here you will have to put the image name for the "small" icon.
-        SetDiscordRichPresenceAssetSmall('logo_name')
+        SetDiscordRichPresenceAssetSmall('nrsmalllogotrans')
 
         -- Here you can add hover text for the "small" icon.
-        SetDiscordRichPresenceAssetSmallText('This is a lsmall icon with text')
+        SetDiscordRichPresenceAssetSmallText('Get involved today!')
 
         QBCore.Functions.TriggerCallback('smallresources:server:GetCurrentPlayers', function(result)
-            SetRichPresence('Players: '..result..'/64')
+            SetRichPresence('Players: '..result..'/128')
         end)
 
         -- (26-02-2021) New Native:
@@ -32,8 +32,8 @@ CreateThread(function()
             First paramater is the button index (0 or 1), second is the title and
             last is the url (this has to start with "fivem://connect/" or "https://")
         ]]--
-        SetDiscordRichPresenceAction(0, "First Button!", "fivem://connect/localhost:30120")
-        SetDiscordRichPresenceAction(1, "Second Button!", "fivem://connect/localhost:30120")
+        SetDiscordRichPresenceAction(0, "Join Discord", "https://www.dsc.gg/near-reality")
+        SetDiscordRichPresenceAction(1, "Join FiveM", "fivem://connect/147.189.170.236:30120")
 
         -- It updates every minute just in case.
         Wait(60000)
