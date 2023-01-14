4a7f32269314e788cfde8e353e2bad76c6e9ca6e
Moley93 <joshmole@me.com>
2023-01-14 14:52:13 +0000

    Discord Rich Presence Update

--- [standalone]/discord/client.lua
+++ [standalone]/discord/client.lua
@@ -25,7 +25,7 @@ Citizen.CreateThread(function()
             First paramater is the button index (0 or 1), second is the title and 
             last is the url (this has to start with "fivem://connect/" or "https://") 
         ]]--
-        SetDiscordRichPresenceAction(0, "Join Discord!", "https://dsg.gg/near-reality")
+        SetDiscordRichPresenceAction(0, "Join Discord!", "https://discord.gg/near-reality")
         SetDiscordRichPresenceAction(1, "Join Server!", "fivem://147.189.170.236:30120")
 
         -- It updates every minute just in case.
