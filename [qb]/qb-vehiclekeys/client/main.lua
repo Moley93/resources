f8d5aa7655cb0fb1912d3bd0ba198ce1f010373b
Moley93 <joshmole@me.com>
2023-01-17 21:01:45 +0000

    Revert "Revert "QOL Changes""
    This reverts commit 83c15efd4c9852a122f0bbc3de028327c5376fa6.

--- [qb]/qb-vehiclekeys/client/main.lua
+++ [qb]/qb-vehiclekeys/client/main.lua
@@ -93,12 +93,12 @@ CreateThread(function()
                     sleep = 0
 
                     local vehiclePos = GetOffsetFromEntityInWorldCoords(vehicle, 0.0, 1.0, 0.5)
-                    DrawText3D(vehiclePos.x, vehiclePos.y, vehiclePos.z, Lang:t("info.skeys"))
+--                    DrawText3D(vehiclePos.x, vehiclePos.y, vehiclePos.z, Lang:t("info.skeys"))
                     SetVehicleEngineOn(vehicle, false, false, true)
 
-                    if IsControlJustPressed(0, 74) then
-                        Hotwire(vehicle, plate)
-                    end
+--                    if IsControlJustPressed(0, 74) then
+--                        Hotwire(vehicle, plate)
+--                    end
                 end
             end
 
