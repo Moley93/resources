5a1ad0f551d46366cca11251a95dc1e8e7910a6b
Moley93 <joshmole@me.com>
2023-01-17 22:46:03 +0000

    Test

--- [qb]/qb-vehiclekeys/config.lua
+++ [qb]/qb-vehiclekeys/config.lua
@@ -1,9 +1,9 @@
 Config = {}
 
 -- NPC Vehicle Lock States
-Config.LockNPCDrivingCars = false -- Lock state for NPC cars being driven by NPCs [true = locked, false = unlocked]
+Config.LockNPCDrivingCars = true -- Lock state for NPC cars being driven by NPCs [true = locked, false = unlocked]
 Config.LockNPCParkedCars = true -- Lock state for NPC parked cars [true = locked, false = unlocked]
-
+Config.UseKeyfob = false -- you can set this true if you dont need ui
 -- Lockpick Settings
 Config.RemoveLockpickNormal = 0.5 -- Chance to remove lockpick on fail
 Config.RemoveLockpickAdvanced = 0.2 -- Chance to remove advanced lockpick on fail
@@ -16,7 +16,7 @@ Config.CarJackEnable = true -- True allows for the ability to car jack peds.
 Config.CarjackingTime = 7500 -- How long it takes to carjack
 Config.DelayBetweenCarjackings = 10000 -- Time before you can carjack again
 Config.CarjackChance = {
-    ['2685387236'] = 0.1, -- melee
+    ['2685387236'] = 0.0, -- melee
     ['416676503'] = 0.5, -- handguns
     ['-957766203'] = 0.75, -- SMG
     ['860033945'] = 0.90, -- shotgun
--- [qb]/qb-vehiclekeys/fxmanifest.lua
+++ [qb]/qb-vehiclekeys/fxmanifest.lua
@@ -1,18 +1,19 @@
-server_script "STU827C35.lua"
-client_script "STU827C35.lua"
 fx_version 'cerulean'
 game 'gta5'
-
 description 'QB-VehicleKeys'
-version '1.0.0'
-
+version '1.2.0'
+ui_page 'NUI/index.html'
+files {
+    'NUI/index.html',
+    'NUI/style.css',
+    'NUI/script.js',
+    'NUI/images/*',
+}
 shared_scripts {
     '@qb-core/shared/locale.lua',
-    'locales/en.lua',
     'locales/*.lua',
     'config.lua',
 }
 client_script 'client/main.lua'
 server_script 'server/main.lua'
-
 lua54 'yes'
