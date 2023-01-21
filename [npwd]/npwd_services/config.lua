0c6f328b507d2c11467dbb4d28a32a04924a9139
Moley93 <joshmole@me.com>
2023-01-21 11:59:15 +0000

    NPWD Services Contact

--- [npwd]/npwd_services/config.lua
+++ [npwd]/npwd_services/config.lua
@@ -1,7 +1,19 @@
 Config = {}
 
 Config.Jobs = {
+    "ambulance",
+    "police",
+    "judge",
+    "lawyer",
+    "lsmech",
+    "r68mech",
+    "paletomech",
+    "ottomech",
+    "cardealer",
+    "realestate",
+    "taxi",
     "tow",
+    "reporter",
 }
 
 Config.ShowOffDuty = false --show players who aren't currently on duty (QBcore Only)
